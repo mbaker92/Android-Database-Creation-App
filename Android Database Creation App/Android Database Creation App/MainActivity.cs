17c7c629113024df9aaf4c289d653eb531ae4141
Matthew Baker <hackmattr@gmail.com>
2017-09-04 17:21:27 -0700

    Initial Commit
    A simple android app that creates a database based on user input and
    then copies the database to the Downloads folder of the Android System
    for easy retrieval.

---
+++ Android Database Creation App/Android Database Creation App/MainActivity.cs
@@ -0,0 +1,92 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using SQLite;
+using System.IO;
+using Android.Content;
+
+namespace Android_Database_Creation_App
+{
+    [Activity(Label = "Android_Database_Creation_App", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        
+        private EditText CategoryField;
+        private EditText QuestionField;
+        private EditText AnswerField;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+
+            // Get the Create Button and Add Button on Main Screen
+            Button AddButton = FindViewById<Button>(Resource.Id.addToDB);
+            Button CreateButton = FindViewById<Button>(Resource.Id.createButton);
+
+
+            // Get Fields that User will use
+            CategoryField = FindViewById<EditText>(Resource.Id.categoryField);
+            QuestionField = FindViewById<EditText>(Resource.Id.questionField);
+            AnswerField = FindViewById<EditText>(Resource.Id.answerField);
+
+            // Get the Database Path on the system
+            string DBPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "DatabaseCreated.db3");
+
+            // If there is already a DatabaseCreated.db3 file, then delete it
+            if (System.IO.File.Exists(DBPath))
+            {
+                System.IO.File.Delete(DBPath);
+            }
+
+            // Create the Connection
+            var DB = new SQLiteConnection(DBPath);
+
+            // Create a Table using the Question Class
+            DB.CreateTable<Question>();
+
+
+            AddButton.Click += delegate
+            {
+                // If all fields have text then add information to the database
+                if((AnswerField.Text != "") && (QuestionField.Text != "") && (CategoryField.Text != ""))
+                {
+                    Question temp = new Question(CategoryField.Text, QuestionField.Text, AnswerField.Text);
+                    DB.Insert(temp);
+                }
+
+
+                // Reset the fields to blank
+                AnswerField.Text = "";
+                QuestionField.Text = "";
+                CategoryField.Text = "";
+
+            };
+
+
+            CreateButton.Click += delegate
+            {
+
+                // Close the database
+                DB.Close();
+
+                // Get path to the Android Download Directory
+                var backup = Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryDownloads);
+
+                // Get the fullpath with filename of Android Download Directory
+                string DownloadPath = Path.Combine(backup.AbsolutePath, "DatabaseCreated.db3");
+
+                // Copy Database to the Download Directory. If there is already a DatabaseCreated.db3 file, then overwrite it.
+                System.IO.File.Copy(DBPath, DownloadPath, true);
+
+            };
+
+
+        }
+
+    }
+}
+
---
+++ Android Database Creation App/Android Database Creation App/Question.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+
+namespace Android_Database_Creation_App
+{
+    public class Question
+    {
+        [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+        public string Category { get; set; }
+        public string Questions { get; set; }
+        public string Answer { get; set; }
+
+
+
+        public Question(string cat, string question, string answer)
+        {
+
+            Category = cat;
+            Questions = question;
+            Answer = answer;
+        }
+
+        public Question()
+        {
+
+        }
+    }
+
+  
+}
